4adf56ad8e445e2f3bf26913f52589bbe685bacf
Ansh <anshjangra1234@example.com>
2026-09-10 16:55:18 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ansh",
+    author_email="anshjangra1234@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
